748e64fbf8adcf306f6f0e5f24c98f8848eaa2b6
Young998904 <like0998904@gmail.com>
2022-11-01 10:20:48 +0900

    :tada: 프로젝트 세팅

---
+++ src/main/java/com/ay/study/mybatis_1101/Mybatis1101Application.java
@@ -0,0 +1,13 @@
+package com.ay.study.mybatis_1101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybatis1101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mybatis1101Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ay/study/mybatis_1101/Mybatis1101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ay.study.mybatis_1101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybatis1101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
