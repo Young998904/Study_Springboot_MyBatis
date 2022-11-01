a14ad8d0ab1cc3cf2c4c19af9bc206c743375635
Young998904 <like0998904@gmail.com>
2022-11-01 13:14:00 +0900

    :rewind: 이전 상태로 돌림 (Mapper 와 Select 활용 상태)

--- src/main/java/com/ay/study/mybatis_1101/app/article/repository/ArticleRepository.java
+++ src/main/java/com/ay/study/mybatis_1101/app/article/repository/ArticleRepository.java
@@ -3,8 +3,15 @@ package com.ay.study.mybatis_1101.app.article.repository;
 import com.ay.study.mybatis_1101.app.article.dto.Article;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
-public interface ArticleMapper {
+public interface ArticleRepository {
+    @Select("""
+        <script>
+        SELECT *
+        FROM article
+        </script>
+        """)
     List<Article> getArticles();
 }
--- src/main/java/com/ay/study/mybatis_1101/app/article/service/ArticleService.java
+++ src/main/java/com/ay/study/mybatis_1101/app/article/service/ArticleService.java
@@ -1,7 +1,7 @@
 package com.ay.study.mybatis_1101.app.article.service;
 
 import com.ay.study.mybatis_1101.app.article.dto.Article;
-import com.ay.study.mybatis_1101.app.article.repository.ArticleMapper;
+import com.ay.study.mybatis_1101.app.article.repository.ArticleRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,9 +9,9 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
-    private final ArticleMapper articleMapper;
+    private final ArticleRepository articleRepository;
 
     public List<Article> getArticles() {
-        return articleMapper.getArticles();
+        return articleRepository.getArticles();
     }
 }
