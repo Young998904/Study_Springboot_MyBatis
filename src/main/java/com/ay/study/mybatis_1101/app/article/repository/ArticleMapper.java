50ef0b392b92a619777c0426e2bc5585a4e2a885
Young998904 <like0998904@gmail.com>
2022-11-01 13:09:47 +0900

    :white_check_mark: 게시물 리스트 확인 테스트 및 각종 클래스 생성 방법 (2) (xml 활용)

--- src/main/java/com/ay/study/mybatis_1101/app/article/repository/ArticleMapper.java
+++ src/main/java/com/ay/study/mybatis_1101/app/article/repository/ArticleMapper.java
@@ -3,15 +3,8 @@ package com.ay.study.mybatis_1101.app.article.repository;
 import com.ay.study.mybatis_1101.app.article.dto.Article;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 @Mapper
-public interface ArticleRepository {
-    @Select("""
-        <script>
-        SELECT *
-        FROM article
-        </script>
-        """)
+public interface ArticleMapper {
     List<Article> getArticles();
 }
--- src/main/java/com/ay/study/mybatis_1101/app/article/service/ArticleService.java
+++ src/main/java/com/ay/study/mybatis_1101/app/article/service/ArticleService.java
@@ -1,7 +1,7 @@
 package com.ay.study.mybatis_1101.app.article.service;
 
 import com.ay.study.mybatis_1101.app.article.dto.Article;
-import com.ay.study.mybatis_1101.app.article.repository.ArticleRepository;
+import com.ay.study.mybatis_1101.app.article.repository.ArticleMapper;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,9 +9,9 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
-    private final ArticleRepository articleRepository;
+    private final ArticleMapper articleMapper;
 
     public List<Article> getArticles() {
-        return articleRepository.getArticles();
+        return articleMapper.getArticles();
     }
 }
